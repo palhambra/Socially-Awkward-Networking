7819967cbbbb16263a7014a0345c77bef2a14f3b
Paolo <paolo.alhambra@yahoo.com>
2023-05-08 20:10:50 -0700

    fixed index.js mispelling

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -13,7 +13,7 @@ const {
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser).update(updateUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
 
